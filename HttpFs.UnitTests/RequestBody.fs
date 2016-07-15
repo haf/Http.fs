94242485d793f717d5f4f74d71aff859e1074042
Jakob Larsson <larssonoj@gmail.com>
2016-07-15 12:12:24 +0200

    [infra] Fixed indentation of character encoding test

--- HttpFs.UnitTests/RequestBody.fs
+++ HttpFs.UnitTests/RequestBody.fs
@@ -191,18 +191,18 @@ let internals =
 
 [<Tests>]
 let textEncodingTest =
-    let pathOf relativePath =
-        let here = IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)
-        IO.Path.Combine(here, relativePath)
-    testCase "character encoding when files are attached to request" <| fun _ ->
-        let requestBody =
-            Request.create Post (Uri "http://localhost/")
-            |> Request.body (
-                BodyForm [
-                    NameValue("Special letters", "åäö")
-                    NameValue("More Special letters", "©®™")
-                    FormFile("file", ("pix.gif", ContentType.create("image", "gif"), Binary (System.IO.File.ReadAllBytes(pathOf "pix.gif"))))
-            ])
-        let rawBodyString = DotNetWrapper.getRawRequestBodyString HttpFsState.empty requestBody
-        Assert.StringContains("", "åäö", rawBodyString)
-        Assert.StringContains("", "©®™", rawBodyString)
+  let pathOf relativePath =
+    let here = IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)
+    IO.Path.Combine(here, relativePath)
+  testCase "character encoding when files are attached to request" <| fun _ ->
+    let requestBody =
+      Request.create Post (Uri "http://localhost/")
+        |> Request.body (
+          BodyForm [
+            NameValue("Special letters", "åäö")
+            NameValue("More Special letters", "©®™")
+            FormFile("file", ("pix.gif", ContentType.create("image", "gif"), Binary (System.IO.File.ReadAllBytes(pathOf "pix.gif"))))
+        ])
+    let rawBodyString = DotNetWrapper.getRawRequestBodyString HttpFsState.empty requestBody
+    Assert.StringContains("", "åäö", rawBodyString)
+    Assert.StringContains("", "©®™", rawBodyString)
