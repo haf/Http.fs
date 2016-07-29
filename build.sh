361e42e2eb13357e5c4ec0d5adfb8c77a36ba325
Henrik Feldt <henrik@haf.se>
2016-07-29 18:17:12 +0200

    [infra] removing old build infra

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-if [[ ! -f .paket/paket.exe ]]; then
-  mono Tools/paket.bootstrapper.exe
-fi
-mono Tools/paket.exe restore
-mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
